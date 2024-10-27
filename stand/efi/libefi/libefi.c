391e3abc679822ccb3397fadc532fa6d5ff03466
Haowu Ge <gehaowu@bitmoe.com>
2024-10-27 14:45:59 +0800

    追加stand/efi/libefi/libefi.c的LoongArch支持，抄抄改改，补丁来自yushanwei/hitmoon两位大佬

--- stand/efi/libefi/libefi.c
+++ stand/efi/libefi/libefi.c
@@ -111,3 +111,31 @@ OpenProtocolByHandle(EFI_HANDLE handle, EFI_GUID *protocol, void **interface)
 	return (BS->OpenProtocol(handle, protocol, interface, IH, NULL,
 	    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL));
 }
+
+void *GetFdtHob(EFI_HOB_GENERIC_HEADER *HobStart) {
+        EFI_HOB_GENERIC_HEADER *CurrentHob = HobStart;
+        EFI_HOB_GUID_TYPE *guid;
+        EFI_GUID *id;
+        EFI_GUID fdt = FDT_HOB_GUID;
+        UINT64 FdtBase;
+
+        while (CurrentHob->HobType != EFI_HOB_TYPE_END_OF_HOB_LIST) {
+                switch (CurrentHob->HobType) {
+                case EFI_HOB_TYPE_GUID_EXTENSION:
+                        guid = (EFI_HOB_GUID_TYPE *)CurrentHob;
+                        id = &guid->Name;
+                        if (!memcmp(id, &fdt, sizeof(EFI_GUID))) {
+                                FdtBase = *(UINT64 *)((UINT8 *)CurrentHob + sizeof (EFI_HOB_GUID_TYPE));
+                                return (void*)FdtBase;
+                        }
+                        break;
+                default:
+                        break;
+                }
+
+                // Move to the next HOB in the list
+                CurrentHob = (EFI_HOB_GENERIC_HEADER *)((char *)CurrentHob + CurrentHob->HobLength);
+        }
+
+        return NULL;
+}
