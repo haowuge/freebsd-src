28aeb978331479587cf3b310640b036ff708c777
Haowu Ge <gehaowu@bitmoe.com>
2024-10-27 22:52:52 +0800

    add stand/efi/loader/arch/loongarch

--- stand/efi/loader/arch/loongarch/exec.c
+++ stand/efi/loader/arch/loongarch/exec.c
@@ -25,6 +25,7 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/cdefs.h>
 #include <sys/param.h>
 #include <sys/linker.h>
 
@@ -36,6 +37,7 @@
 
 #include <efi.h>
 #include <efilib.h>
+#include <Protocol/MpService.h>
 
 #include "bootstrap.h"
 #include "loader_efi.h"
@@ -50,6 +52,10 @@ __elfN(exec)(struct preloaded_file *fp)
 	Elf_Ehdr *e;
 	int error;
 	void (*entry)(void *);
+	UINTN cpu_num, cpu_enum;
+	EFI_MP_SERVICES_PROTOCOL *mp_service;
+	EFI_STATUS Status;
+	EFI_GUID mps = EFI_MP_SERVICES_PROTOCOL_GUID;
 
 	if ((fmp = file_findmetadata(fp, MODINFOMD_ELFHDR)) == NULL)
 		return (EFTYPE);
@@ -60,8 +66,18 @@ __elfN(exec)(struct preloaded_file *fp)
 
 	entry = efi_translate(e->e_entry);
 
-	printf("Kernel entry at %p...\n", entry);
-	printf("Kernel args: %s\n", fp->f_args);
+	Status = BS->LocateProtocol(&mps, NULL, (VOID**)&mp_service);
+
+	if (EFI_ERROR(Status)) {
+		printf("Failed to locate MP Service Protocol.\n");
+	}
+
+	Status = mp_service->GetNumberOfProcessors(mp_service, &cpu_num, &cpu_enum);
+	if (EFI_ERROR(Status)) {
+		printf("Failed to get number of processors\n");
+	}
+
+	printf("Total processors: %lu, Enabled processors: %lu\n", cpu_num, cpu_enum);
 
 	if ((error = bi_load(fp->f_args, &modulep, &kernend, true)) != 0) {
 		efi_time_init();
@@ -78,12 +94,12 @@ __elfN(exec)(struct preloaded_file *fp)
 	panic("exec returned");
 }
 
-static struct file_format riscv_elf = {
+static struct file_format loongarch_elf = {
 	__elfN(loadfile),
 	__elfN(exec)
 };
 
 struct file_format *file_formats[] = {
-	&riscv_elf,
+	&loongarch_elf,
 	NULL
 };
