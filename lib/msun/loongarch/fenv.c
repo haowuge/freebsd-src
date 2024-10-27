8d77be443e99bca7efbb31ca1476416a8d8b8362
Haowu Ge <gehaowu@bitmoe.com>
2024-10-27 09:28:48 +0800

    追加lib/msun/loongarch的LoongArch支持，抄抄改改，补丁来自yushanwei/hitmoon两位大佬

--- lib/msun/loongarch/fenv.c
+++ lib/msun/loongarch/fenv.c
@@ -37,25 +37,17 @@
  */
 const fenv_t __fe_dfl_env = 0;
 
-#ifdef __riscv_float_abi_soft
-#define __set_env(env, flags, mask, rnd) env = ((flags) | (rnd) << 5)
-#define __env_flags(env)                ((env) & FE_ALL_EXCEPT)
-#define __env_mask(env)                 (0) /* No exception traps. */
-#define __env_round(env)                (((env) >> 5) & _ROUND_MASK)
-#include "fenv-softfloat.h"
-#endif
-
-extern inline int feclearexcept(int __excepts);
-extern inline int fegetexceptflag(fexcept_t *__flagp, int __excepts);
-extern inline int fesetexceptflag(const fexcept_t *__flagp, int __excepts);
-extern inline int feraiseexcept(int __excepts);
-extern inline int fetestexcept(int __excepts);
+extern inline int feclearexcept(int excepts);
+extern inline int fegetexceptflag(fexcept_t *flagp, int excepts);
+extern inline int fesetexceptflag(const fexcept_t *flagp, int excepts);
+extern inline int feraiseexcept(int excepts);
+extern inline int fetestexcept(int excepts);
 extern inline int fegetround(void);
-extern inline int fesetround(int __round);
-extern inline int fegetenv(fenv_t *__envp);
-extern inline int feholdexcept(fenv_t *__envp);
-extern inline int fesetenv(const fenv_t *__envp);
-extern inline int feupdateenv(const fenv_t *__envp);
-extern inline int feenableexcept(int __mask);
-extern inline int fedisableexcept(int __mask);
+extern inline int fesetround(int round);
+extern inline int fegetenv(fenv_t *envp);
+extern inline int feholdexcept(fenv_t *envp);
+extern inline int fesetenv(const fenv_t *envp);
+extern inline int feupdateenv(const fenv_t *envp);
+extern inline int feenableexcept(int mask);
+extern inline int fedisableexcept(int mask);
 extern inline int fegetexcept(void);
