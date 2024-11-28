d38dd655f888d9408ef158efa6115b4aad1edb61
Haowu Ge <gehaowu@bitmoe.com>
2024-11-28 20:36:39 +0800

    add sys/loongarch

---
+++ sys/loongarch/include/trap.h
@@ -0,0 +1,10 @@
+
+#ifndef __LOONGARCH_TRAP_H__
+#define __LOONGARCH_TRAP_H__
+#define RSIZE	8														// 64 bit mode register size
+#define GP_REG_CONTEXT_SIZE		32 * RSIZE	// general purpose registers
+#define FP_REG_CONTEXT_SIZE		34 * RSIZE	// Floating-point registers
+#define CSR_REG_CONTEXT_SIZE	9 * RSIZE		// CSR registers
+
+
+#endif
